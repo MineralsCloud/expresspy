1e7dc58740cacdb071dd6441847f97495bd58b95
Qi Zhang <singularitti@outlook.com>
2019-06-27 19:52:15 -0400

    Implement class `AtomicSpecies`

--- src/expresso/cards.py
+++ src/expresso/cards.py
@@ -2,9 +2,34 @@
 # -*- coding: utf-8 -*-
 
 
+from typing import List
+
+from attr import attrs, attrib
+from crystals import Element
+
+
 class Card(object):
     ...
 
 
+@attrs
 class AtomicSpecies(Card):
+    atoms: List[Element] = attrib(factory=list)
+    pseudopotentials: List[str] = attrib(factory=list)
+
+    @pseudopotentials.validator
+    def _check_length(self, attribute, value):
+        if not len(value) == len(self.atoms):
+            raise ValueError("Length mismatch!")
+
+    def __iter__(self):
+        yield self.atoms
+        yield self.pseudopotentials
+
+
+class AtomicPosition(Card):
+    ...
+
+
+class CellParameters(Card):
     ...
