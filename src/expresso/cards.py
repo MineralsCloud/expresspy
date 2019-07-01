9a3cace639bfa33a9e5598402f8897c5c590384d
Qi Zhang <singularitti@outlook.com>
2019-07-01 00:22:42 -0400

    Implement `KPoints` class

--- src/expresso/cards.py
+++ src/expresso/cards.py
@@ -53,3 +53,29 @@ class KPoints(Card):
     _allowed_options = ("tpiba", "automatic", "crystal", "gamma", "tpiba_b", "crystal_b", "tpiba_c", "crystal_c")
     option: str = attrib(default="tpiba", validator=attr.validators.in_(_allowed_options))
     points: List = attrib(factory=list)
+
+    @points.validator
+    def _check_points(self, attribute, value):
+        if self.option == "automatic":
+            if not len(value) == 6:
+                raise ValueError
+
+    @property
+    def mesh(self):
+        if not self.option == "automatic":
+            raise ValueError
+        return self.points[0:3]
+
+    @property
+    def shift(self):
+        if not self.option == "automatic":
+            raise ValueError
+        return self.points[3:6]
+
+    def __len__(self) -> Optional[int]:
+        if self.option == "automatic":
+            return None
+        if self.option in ("tpiba", "crystal", "tpiba_b", "crystal_b", "tpiba_c", "crystal_c"):
+            return self.points.__len__()
+        if self.option == "gamma":
+            return 1
