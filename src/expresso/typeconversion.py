9141f10cf0680a63622b18bfe229bb5a9ccf741a
Qi Zhang <singularitti@outlook.com>
2019-07-01 01:18:32 -0400

    Simplify code: type checking is not needed in single dispatch

--- src/expresso/typeconversion.py
+++ src/expresso/typeconversion.py
@@ -16,29 +16,21 @@ def to_fortran(v):
 
 @to_fortran.register(int)
 def _(v: int) -> str:
-    if not isinstance(v, int):
-        raise TypeError("Expected input to be a int!")
     return str(v)
 
 
 @to_fortran.register(float)
 def _(v: float) -> str:
-    if not isinstance(v, float):
-        raise TypeError("Expected input to be a float!")
     return "{:.16E}".format(v)
 
 
 @to_fortran.register(complex)
 def _(v: complex) -> str:
-    if not isinstance(v, complex):
-        raise TypeError("Expected input to be a complex!")
     return "CMPLX({}, {})".format(to_fortran(v.real), to_fortran(v.imag))
 
 
 @to_fortran.register(bool)
 def _(v: bool) -> str:
-    if not isinstance(v, bool):
-        raise TypeError("Expected input to be a boolean!")
     if v is True:
         return ".true."
     return ".false."
@@ -46,8 +38,6 @@ def _(v: bool) -> str:
 
 @to_fortran.register(str)
 def _(v: str) -> str:
-    if not isinstance(v, str):
-        raise TypeError("Expected input to be a string!")
     return v
 
 
