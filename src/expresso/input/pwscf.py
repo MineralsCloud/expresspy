3469e87f221bbac9de3767a0bcd23f86774a7251
Qi Zhang <singularitti@outlook.com>
2019-07-01 23:09:02 -0400

    Add `cards` validator for `PWscfInput`

--- src/expresso/input/pwscf.py
+++ src/expresso/input/pwscf.py
@@ -25,3 +25,12 @@ class PWscfInput(object):
         if not all([isinstance(value["CONTROL"], ControlNamelist), isinstance(value["SYSTEM"], SystemNamelist),
                     isinstance(value["ELECTRONS"], ElectronsNamelist)]):
             raise ValueError
+
+    @cards.validator
+    def _check_contains(self, attribute, value):
+        if {"ATOMIC_SPECIES", "ATOMIC_POSITIONS", "K_POINTS"}.difference(set(value.keys())) is not {}:
+            raise ValueError
+        if not all([isinstance(value["ATOMIC_SPECIES"], AtomicSpecies),
+                    isinstance(value["ATOMIC_POSITIONS"], AtomicPosition),
+                    isinstance(value["K_POINTS"]), KPoints]):
+            raise ValueError
