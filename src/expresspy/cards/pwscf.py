b1621b1d21cfd010a9cb5779b5cb756bd5855898
Qi Zhang <singularitti@outlook.com>
2019-07-10 01:47:41 -0400

    Move `Card` base class to base.py

--- src/expresspy/cards/pwscf.py
+++ src/expresspy/cards/pwscf.py
@@ -2,19 +2,149 @@
 # -*- coding: utf-8 -*-
 
 
+import re
 import textwrap
-from typing import Optional
+from typing import List, Optional
 
 import attr
+import numpy as np
 from attr import attrib, attrs
+from crystals import Element, Atom, Lattice
 from expresspy.cards.base import Card
 from singleton_decorator import singleton
 
 __all__ = [
+    'LatticeParameters',
     'SpecialKPoint',
     'MonkhorstPackGrid',
-    'KPoints'
+    'KPoints',
+    'AtomicSpecies',
+    'AtomicPosition',
+    'CellParameters'
 ]
+
+
+@attrs(frozen=True)
+class LatticeParameters(object):
+    a = attrib(converter=float)
+    b = attrib(converter=float)
+    c = attrib(converter=float)
+    alpha = attrib(converter=float)
+    beta = attrib(converter=float)
+    gamma = attrib(converter=float)
+
+    @property
+    def edges(self):
+        return [self.a, self.b, self.c]
+
+    @property
+    def angles(self):
+        return [self.alpha, self.beta, self.gamma]
+
+    def to_tuple(self):
+        return attr.astuple(self)
+
+    def __getitem__(self, item):
+        return self.to_tuple().__getitem__(item)
+
+    def __len__(self):
+        return self.to_tuple().__len__()
+
+
+@attrs
+class AtomicSpecies(Card):
+    _name: str = "ATOMIC_SPECIES"
+    atoms: List[Element] = attrib(factory=list)
+    masses: List[float] = attrib(factory=list)
+    pseudopotentials: List[str] = attrib(factory=list)
+
+    @masses.validator
+    def _check_length_match(self, attribute, value):
+        if not len(value) == len(self.atoms):
+            raise ValueError("Length mismatch!")
+
+    @pseudopotentials.validator
+    def _check_length_match(self, attribute, value):
+        if not len(value) == len(self.atoms):
+            raise ValueError("Length mismatch!")
+
+    @property
+    def data(self):
+        return list(zip(self.atoms, self.masses, self.pseudopotentials))
+
+    def __getitem__(self, item):
+        return self.data.__getitem__(item)
+
+    def __len__(self):
+        return self.data.__len__()
+
+    def to_qe(self):
+        return textwrap.dedent(f"""\
+        {self._name}
+        """ + f"{x}" for x in self.data)
+
+
+@attrs
+class AtomicPosition(Card):
+    _name: str = "ATOMIC_POSITIONS"
+    _allowed_options = ("alat", "bohr", "angstrom", "crystal", "crystal_sg")
+    option: str = attrib(default="alat", validator=attr.validators.in_(_allowed_options))
+    atoms: List[Atom] = attrib(factory=list)
+
+    def to_qe(self):
+        return textwrap.dedent(f"""\
+        {self._name} {self.option}
+        """ + f"{x}" for x in self.atoms)
+
+
+@attrs
+class CellParameters(Card):
+    _name: str = "CELL_PARAMETERS"
+    _allowed_options = ("alat", "bohr", "angstrom")
+    option: str = attrib(default="alat", validator=attr.validators.in_(_allowed_options))
+    lattice: Lattice = attrib(default=Lattice(np.diag([1, 1, 1])))
+
+    @classmethod
+    def from_parameters(cls, option: str, a: float, b: float, c: float, alpha: float, beta: float, gamma: float):
+        return cls(option, Lattice.from_parameters(a, b, c, alpha, beta, gamma))
+
+    @classmethod
+    def from_array(cls, option: str, array: np.ndarray):
+        if not array.shape == (3, 3):
+            raise ValueError(f"Expected array of shape (3, 3), given {array.shape}!")
+        return cls(option, Lattice(array))
+
+    @property
+    def lattice_parameters(self):
+        return LatticeParameters(*self.lattice.lattice_parameters)
+
+    @property
+    def lattice_system(self):
+        return Lattice.from_parameters(*self.lattice_parameters.to_tuple()).lattice_system
+
+    @property
+    def lattice_vectors(self):
+        return Lattice.from_parameters(*self.lattice_parameters.to_tuple()).lattice_vectors
+
+    @property
+    def periodicity(self):
+        return Lattice.from_parameters(*self.lattice_parameters.to_tuple()).periodicity
+
+    @property
+    def reciprocal_lattice(self):
+        return Lattice.from_parameters(*self.lattice_parameters.to_tuple()).reciprocal
+
+    @property
+    def reciprocal_vectors(self):
+        return Lattice.from_parameters(*self.lattice_parameters.to_tuple()).reciprocal_vectors
+
+    @property
+    def volume(self):
+        return Lattice.from_parameters(*self.lattice_parameters.to_tuple()).volume
+
+    def to_qe(self):
+        return re.sub("[\[\]]", ' ',
+                      np.array2string(self.lattice_vectors, formatter={'float_kind': lambda x: "{:20.10f}".format(x)}))
 
 
 @attrs
